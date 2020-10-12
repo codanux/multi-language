bb94125a207d46d61ffbf5b823b1489265e35967
omer <omer@optimumtasarim.com>
2020-10-12 13:54:03 +0300

    generate uri default name

--- src/MultiLanguage.php
+++ src/MultiLanguage.php
@@ -2,11 +2,15 @@
 
 namespace Codanux\MultiLanguage;
 
+use Illuminate\Support\Facades\Lang;
+
 class MultiLanguage
 {
     public static function generateUri(string $name, string $locale, bool $prefix = true)
     {
-        $uri = trans("routes.{$name}", [], $locale);
+        $key = "routes.{$name}";
+
+        $uri  = Lang::has($key) ? Lang::get($key, [], $locale) : $name;
 
         if ($prefix && (config('multi-language.default_prefix') ||
             ! ($locale == config('multi-language.default_locale'))))
